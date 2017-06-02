89158b3037c0dc12ccdffcc15f250525afa3b098
NickNordale <nicknordale@gmail.com>
2017-06-01 20:23:28 -0700

    Us map n stuff

--- js/USmap.js
+++ js/USmap.js
@@ -54,7 +54,7 @@ let stateMap = {
     "West Virginia": 54,
     "Wisconsin": 55,
     "Wyoming": 56,
-}
+};
 
 $(function () {
 
@@ -74,19 +74,77 @@ $(function () {
     let svg = d3.select("#vis-us")
         .append("svg")
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", height)
+        .attr("transform", "translate(0,0)");
 
     let colorScale = d3.scaleLog();
 
-    d3.queue()
-        .defer(d3.json, "https://d3js.org/us-10m.v1.json")
-        .defer(d3.csv, "./data/prep/USmap.csv", function (d) {
+    let yeardomain = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
 
-            if (d["State or territory of residence"] in stateMap) {
-                map.set(padInt(stateMap[d["State or territory of residence"]]), parseInt(d[2006].replace(/,/g, "")))
-            }
-        })
-        .await(ready);
+    let margin = {top: 20, right: 20, bottom: 20, left: 20},
+        filterwidth = 1200 - margin.left - margin.right,
+        filterheight = 90 - margin.top - margin.bottom;
+
+    let xScale = d3.scaleLinear()
+        .domain([d3.min(yeardomain), d3.max(yeardomain)])
+        .range([0, filterwidth]);
+
+    let xAxis = d3.axisBottom().scale(xScale);
+
+    let svgfilter = d3.select("#svg-filter")
+        .attr("width", filterwidth + margin.left + margin.right)
+        .attr("height", filterheight);
+
+    svgfilter.append("g")
+        .attr("class", "axis axis--grid")
+        .attr("transform", "translate(0," + filterheight + ")")
+        .call(d3.axisBottom().scale(xScale).tickSize(-filterheight).tickFormat(function() { return null; }));
+
+    let xWidth = xScale(yeardomain[1]) - xScale(yeardomain[0]);
+
+    svgfilter.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(0," + 0 + ")")
+        .call(xAxis.tickSize(filterheight).tickSize(0).tickFormat(d3.format("d")))
+        .attr("text-anchor", null)
+        .selectAll("text")
+            .attr("x", xWidth / 2.0)
+            .attr("y", (filterheight / 2) - 2)
+            .style("text-anchor", "middle");
+
+    svgfilter.append("g")
+        .attr("class", "brush")
+        .call(d3.brushX()
+            .extent([[0, 0], [filterwidth, filterheight]])
+            .on("end", brushed))
+        .call(d3.brushX().move, [2009, 2013].map(xScale));
+
+    function brushed() {
+        if (!d3.event.sourceEvent) return;
+        if (!d3.event.selection) return;
+        let d0 = d3.event.selection.map(xScale.invert),
+            d1 = d0.map(Math.round);
+
+        if (d1[0] >= d1[1]) {
+            d1[0] = Math.floor(d0[0]);
+            d1[1] = d1[0] + 1;
+        }
+
+        d3.select(this).transition().call(d3.event.target.move, d1.map(xScale));
+        drawData([...Array(d1[1] - d1[0]).keys()].map(x => x + d1[0]))
+    }
+
+    function drawData(cols) {
+        d3.queue()
+            .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+            .defer(d3.csv, "./data/prep/USmap.csv", function (d) {
+
+                if (d["State or territory of residence"] in stateMap) {
+                    map.set(padInt(stateMap[d["State or territory of residence"]]), cols.map(x => parseInt(d[x].replace(/,/g, ""))).reduce((a, b) => a + b, 0))
+                }
+            })
+            .await(ready);
+    }
 
     function ready(error, us) {
         let min = d3.min(map.values());
@@ -107,7 +165,7 @@ $(function () {
             .attr("class", "legendQuant")
             .attr("transform", "translate(1000,100)");
 
-        var legend = d3.legendColor()
+        let legend = d3.legendColor()
             .title("# Persons Obtaining Lawful Permanent Resident")
             .labelFormat(d3.format(".0f"))
             .labels(d3.legendHelpers.thresholdLabels)
@@ -116,4 +174,6 @@ $(function () {
         svg.select(".legendQuant")
             .call(legend);
     }
+
+    drawData([2009, 2010, 2011, 2012]);
 });
